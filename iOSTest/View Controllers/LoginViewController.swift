08ff01102b2a60e7524bc6c34921d1da5f034b92
Nitisha Bhandari <nitisha@neuroleap.com>
2020-05-27 18:10:39 -0400

    Add title to LoginViewController

--- iOSTest/View Controllers/LoginViewController.swift
+++ iOSTest/View Controllers/LoginViewController.swift
@@ -33,6 +33,8 @@ class LoginViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Login"
     }
     
     // MARK: - Actions
