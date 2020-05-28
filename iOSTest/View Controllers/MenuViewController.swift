0cf6a2152a4103208821f5655769269a2b8d7a7c
Miguel Tepale <miguel.tepale@gmail.com>
2020-05-28 02:42:47 -0400

    Update Navigation Bar properties

--- iOSTest/View Controllers/MenuViewController.swift
+++ iOSTest/View Controllers/MenuViewController.swift
@@ -23,13 +23,43 @@ class MenuViewController: UIViewController {
      */
     
     // MARK: - Outlets
-    
+
+    @IBOutlet weak var chatButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var animationButton: UIButton!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
+        title = "Coding Tasks"
+        
+        // Round the corners of all buttons
+        chatButton.layer.cornerRadius = 10.0
+        loginButton.layer.cornerRadius = 10.0
+        animationButton.layer.cornerRadius = 10.0
+        
+        // Set the button insets
+        
+        let imageSpacing = CGFloat(22.0)
+        let titleSpacing = CGFloat(16.0)
+        let netTitleSpacing = imageSpacing + titleSpacing
+        
+        // Add padding to the chat button
+        chatButton.imageEdgeInsets = UIEdgeInsets(top:0.0, left: imageSpacing, bottom: 0.0, right: 0.0)
+        
+        chatButton.titleEdgeInsets = UIEdgeInsets(top:0.0, left: netTitleSpacing, bottom:0.0, right: 0.0)
+        
+        // Add padding to the login button
+        loginButton.imageEdgeInsets = UIEdgeInsets(top:0.0, left: imageSpacing, bottom: 0.0, right: 0.0)
+        
+        loginButton.titleEdgeInsets = UIEdgeInsets(top:0.0, left: netTitleSpacing, bottom:0.0, right: 0.0)
+        
+        // Add padding to the animation button
+        animationButton.imageEdgeInsets = UIEdgeInsets(top:0.0, left: imageSpacing, bottom: 0.0, right: 0.0)
+               
+        animationButton.titleEdgeInsets = UIEdgeInsets(top:0.0, left: netTitleSpacing, bottom:0.0, right: 0.0)
     }
     
     // MARK: - Actions
@@ -39,7 +69,11 @@ class MenuViewController: UIViewController {
         navigationController?.pushViewController(chatViewController, animated: true)
     }
     @IBAction func login(_ sender: UIButton) {
+        let loginViewController = LoginViewController()
+        navigationController?.pushViewController(loginViewController, animated: true)
     }
     @IBAction func animation(_ sender: UIButton) {
+        let animationViewController = AnimationViewController()
+        navigationController?.pushViewController(animationViewController, animated: true)
     }
 }
